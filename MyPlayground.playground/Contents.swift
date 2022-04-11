376f49a93c845914f421019520d79b406543e4a8
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-04-11 01:30:31 -0600

    Day1
    Part 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,79 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Colin Robinson"
+name = "Guillermo"
+let character = "Vlad"
+
+print(name)
+print(character)
+
+let actor = "Chadwick Boseman"
+
+let movie = "Get Out"
+
+let emojiMovie = "🗑"
+
+let nipQuote = """
+\"Marathon not a Sprint\"
+-Nipsey Hussle
+"""
+
+let fileName = "paris.jpg"
+
+print(nipQuote)
+
+print(actor.count)
+
+let movieLength = movie.count
+
+print (actor.uppercased())
+
+print(movie.hasPrefix("Get"))
+
+print(fileName.hasSuffix("jpg"))
+
+let score = 10
+
+let reallyBig = 100_00_0____000
+
+let lowerScore = score - 2
+
+let scoreSquared = score*score
+
+var counter = 20
+
+counter += 5
+
+counter *= 2
+
+let number = 120
+
+print(number.isMultiple(of: 3))
+print(counter)
+
+
+let newNumber = 0.1+0.2
+print(newNumber)
+
+let a = 1
+let b = 2.0
+let c = Double(a)+b
+let d = a+Int(b)
+print(c)
+print(d)
+
+let double1 = 3.1
+let double2 = 23.923
+let double = 4.0
+let int1 = 3
+
+print(double1+double2+double)
+print(int1+Int(double2))
+
+var rating = 13
+rating *= 4
+print(rating)
+
+
