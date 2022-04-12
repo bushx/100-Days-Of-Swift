9dc6332089ce7a52644e57c1bff300a88e76e43c
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-04-11 22:02:49 -0600

    CheckPoint 1
    Temp Converter

---
+++ CheckPoint1TempConverter.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+var greeting = "Hello, This is an extremely basic temperature converter"
+
+let cTemp = 24
+let fTempConverted = (cTemp*9/5)+32
+
+print("Temperature in Celsius is \(cTemp)°C")
+
+print("Temperature in Fahrenheit is \(fTempConverted)°F")
