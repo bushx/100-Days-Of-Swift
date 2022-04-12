04130cd164a1130f1ea1855a5f0ea3527ff5fc20
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-04-11 21:37:52 -0600

    Booleans
    Completed

--- Day2.playground/Contents.swift
+++ Day2.playground/Contents.swift
@@ -1,3 +1,19 @@
 import Cocoa
 
 var greeting = "Hello, Day 2"
+
+let goodDogs = true
+
+let gameOver = false
+print(gameOver)
+gameOver.toggle()
+print(gameOver)
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
