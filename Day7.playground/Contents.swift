a48bba54ffa110280dfb8e559e58b31085ee24d7
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-04-16 22:48:15 -0600

    day 7
    customize parameter labels

--- Day7.playground/Contents.swift
+++ Day7.playground/Contents.swift
@@ -29,11 +29,6 @@ printTimes(number: 2, end: 30)
 let root = sqrt(169)
 print(root)
 
-func rollDice() -> Int {
-    Int.random(in: 1...6)
-}
-let result = rollDice()
-print(result)
 
 
 func areLettersIdentical(string1: String, string2: String) -> Bool {
@@ -48,3 +43,52 @@ let c = pythagoras(a: 3, b: 4)
 print(c)
 
 //////
+
+func getUser() -> (firstName: String, lastName: String) {
+    (firstName: "Taylor", lastName: "Swift")
+}
+
+let user = getUser()
+print("Name: \(user.firstName) \(user.lastName)")
+
+func rollDice(sides: Int, count: Int) -> [Int] {
+    // Start with an empty array
+    var rolls = [Int]()
+
+    // Roll as many dice as needed
+    for _ in 1...count {
+        // Add each result to our array
+        let roll = Int.random(in: 1...sides)
+        rolls.append(roll)
+    }
+
+    // Send back all the rolls
+    return rolls
+}
+
+let rolls = rollDice(sides: 6, count: 4)
+
+print(rolls)
+
+func hireEmployee(name: String) { }
+func hireEmployee(title: String) { }
+func hireEmployee(location: String) { }
+
+let lyric = "I see a red door and I want it painted black"
+print(lyric.hasPrefix("I see"))
+
+func isUppercase(string: String) -> Bool {
+    string == string.uppercased()
+}
+
+let string = "HELLO, WORLD"
+let result = isUppercase(string: string)
+
+
+func printTimesTables(for number: Int) {
+    for i in 1...12 {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5)
