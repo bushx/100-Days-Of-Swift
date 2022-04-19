60d39ee98d97d10ad905d58ecb39837469aa3cdc
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-04-18 22:29:01 -0600

    Day 9
    accept functions as parameters

--- Day9.playground/Contents.swift
+++ Day9.playground/Contents.swift
@@ -64,3 +64,44 @@ let uppercaseTeam = team.map { $0.uppercased() }
 
 print(uppercaseTeam)
 
+
+
+func makeArray(size: Int, using generator: () -> Int) -> [Int]{
+    var numbers = [Int]()
+    
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+    return numbers
+}
+
+let rolls = makeArray(size: 50) {
+    Int.random(in: 1...20)
+}
+
+print(rolls)
+
+func generateNumber() -> Int {
+    Int.random(in: 1...20)
+}
+let newRolls = makeArray(size: 50, using: generateNumber)
+print(newRolls)
+
+func doImportantWork(first: ()-> Void, second: () -> Void, third: () -> Void) {
+    print("First work")
+    first()
+    print("Second work")
+    second()
+    print("Third work")
+    third()
+}
+
+doImportantWork {
+    print("First")
+} second: {
+    print("Second")
+} third: {
+    print("Third")
+}
+
