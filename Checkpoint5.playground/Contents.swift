e3c02c3b921c24e91779a66ae9b7b2901ff2f128
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-04-19 22:15:47 -0600

    day9
    continued cleaning

---
+++ Checkpoint5.playground/Contents.swift
@@ -0,0 +1,31 @@
+import Cocoa
+
+var greeting = "Hello, its checkpoint time!"
+
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+
+
+//for i in 1...luckyNumbers.count {
+ //   if luckyNumbers.a .isMultiple(of: 2){
+  //      luckyNumbers.remove(at: i)
+   // }
+//}
+
+let sortNumbers = luckyNumbers.sorted()
+
+
+for _ in 0..<sortNumbers {
+    if
+    let newNumber = generator()
+    numbers.append(newNumber)
+}
+
+
+
+
+var oddNumbers = sortNumbers
+
+print(luckyNumbers)
+print(sortNumbers)
+print(oddNumbers)
--- Day9.playground/Contents.swift
+++ Day9.playground/Contents.swift
@@ -104,4 +104,4 @@ doImportantWork {
 } third: {
     print("Third")
 }
-
+}
