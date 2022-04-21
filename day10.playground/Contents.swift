89defcd366fdbe411b8a74f91ba193f5712cd2e5
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-04-20 23:31:45 -0600

    day10
    personal structs

---
+++ day10.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
