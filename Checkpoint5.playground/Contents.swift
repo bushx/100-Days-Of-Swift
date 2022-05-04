982718e7e1bdd986db2f405ed9cd638d29dcba88
Bushra Osman <84491178+bushx@users.noreply.github.com>
2022-05-04 14:14:58 -0600

    checkpoint5
    refixed with proper sort and filter
    next is map()

--- Checkpoint5.playground/Contents.swift
+++ Checkpoint5.playground/Contents.swift
@@ -3,29 +3,38 @@ import Cocoa
 var greeting = "Hello, its checkpoint time!"
 
 let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+print(luckyNumbers)
 
 
-
-//for i in 1...luckyNumbers.count {
- //   if luckyNumbers.a .isMultiple(of: 2){
-  //      luckyNumbers.remove(at: i)
-   // }
-//}
+/**
+ You need to use the filter(), sorted(), and map() functions.
+To chain these functions, use luckyNumbers.first { }.second { }, obviously putting the real function calls in there.
+ */
 
 let sortNumbers = luckyNumbers.sorted()
+print(sortNumbers)
+
+let filteredNumbers = sortNumbers.filter {!$0.isMultiple(of: 2)}
+print(filteredNumbers)
 
 
-for _ in 0..<sortNumbers {
-    if
-    let newNumber = generator()
-    numbers.append(newNumber)
+for numbers in filteredNumbers {
+    print(numbers , "is a Lucky Number")
 }
 
+/**
+
+let mapNumbers = filteredNumbers.map {$0 }
+print(mapNumbers)
+
+let mapNumbers = filteredNumbers.map {$0.toString  "is a Lucky Number"}
+print(mapNumbers)
 
 
 
 var oddNumbers = sortNumbers
 
 print(luckyNumbers)
-print(sortNumbers)
+
 print(oddNumbers)
+*/
